3a89263b462f3b30f94805f4e348513f47bba856
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-11-17 19:38:15 +0100

    Estructura proyecto realizada

---
+++ src/app/components/empleados-oficio-component/empleados-oficio-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleados-oficio-component',
+  imports: [],
+  templateUrl: './empleados-oficio-component.html',
+  styleUrl: './empleados-oficio-component.css',
+})
+export class EmpleadosOficioComponent {
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    urlApiEmpleados: "https://apiejemplos.azurewebsites.net/"
+};
